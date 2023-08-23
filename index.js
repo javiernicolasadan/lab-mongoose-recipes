3831d36628dde1ccf9353871b630511ef63eba7c
javiernicolasadan <127655730+javiernicolasadan@users.noreply.github.com>
2023-08-23 20:31:33 +0200

    async/await

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const data = require('./data');
 const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
-mongoose
+/* mongoose
   .connect(MONGODB_URI)
   .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
@@ -32,7 +32,7 @@ mongoose
     })
   })
   .then (() => {
-    const changeRigatoni = async () => {
+      const changeRigatoni = async () => {
       const changeDuration = await Recipe.findOneAndUpdate( {title: "Rigatoni alla Genovese"}, {duration:100}, {new:true})
       console.log(`The duration of ${changeDuration.title} have been changed to ${changeDuration.duration}`)
     }
@@ -48,6 +48,41 @@ mongoose
   .then(() => {
     mongoose.disconnect(MONGODB_URI)
   })
+  .then(() => {
+    console.log("connection closed")
+  })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  }); */
+
+
+  //with async/await
+const manageDB = async () => {
+  try {
+    const connectDB = await mongoose.connect(MONGODB_URI)
+    console.log(`Connected to the database:`,  mongoose.connections[0].name)
+
+    const deleteAllRecipes = await Recipe.deleteMany()
+    console.log(deleteAllRecipes)
+    
+    const newRecipe = await Recipe.create({title: "tortilla", ingredients: ["huevos", "patatas"], cuisine: "whatever", dishType: "main_course", duration: 30, image: "", creator: "javier", created: "today" })
+    console.log(newRecipe.title)
+
+    const allRecipes = await Recipe.insertMany(data)
+    allRecipes.forEach((eachRecipe) => {
+      console.log(eachRecipe.title)
+    })
+
+    const changeDuration = await Recipe.findOneAndUpdate( {title: "Rigatoni alla Genovese"}, {duration:150}, {new:true})
+    console.log(`The duration of ${changeDuration.title} have been changed to ${changeDuration.duration}`)
+
+    const removeCarrot = await Recipe.deleteOne({title:"Carrot Cake"})
+    console.log("Carrot Cake have been removed!")
+
+    mongoose.disconnect(MONGODB_URI)
+    console.log("DB has been disconnected!")
+  } catch (error) {
+    console.log(error)
+  }
+}
+manageDB()
